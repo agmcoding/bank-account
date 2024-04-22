b60c0b3a9215bb9b42a8892a9f4ac10536b1ac89
agmcoding <antonioibmachado@gmail.com>
2024-04-22 14:51:48 -0300

    Chore: Improve readability in Java code
    Indent using 4 spaces pattern and make code style adjustments

--- bank-account/src/main/java/spring/boot/bankaccount/StartApp.java
+++ bank-account/src/main/java/spring/boot/bankaccount/StartApp.java
@@ -14,45 +14,43 @@ import spring.boot.bankaccount.repository.AccountRepository;
 @Component
 public class StartApp implements CommandLineRunner{
 
-	private AccountRepository accountRepository;
-	private AccountHolderRepository accountHolderRepository;
-	
-	@Autowired
-	public void setAccountRepository(AccountRepository accountRepository) {
-		this.accountRepository = accountRepository;
-	}
+    private AccountRepository accountRepository;
+    private AccountHolderRepository accountHolderRepository;
+  
+    @Autowired
+    public void setAccountRepository(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
 
-	@Autowired
-	public void setAccountHolderRepository(AccountHolderRepository accountHolderRepository) {
-		this.accountHolderRepository = accountHolderRepository;
-	}
+    @Autowired
+    public void setAccountHolderRepository(AccountHolderRepository accountHolderRepository) {
+        this.accountHolderRepository = accountHolderRepository;
+    }
 
 
 
-	@Override
-	public void run(String... args) throws Exception {
-		
-		AccountHolder holder1 = new AccountHolder();
-		holder1.setName("Bob");
-		holder1.setBirthday(LocalDate.of(1990, 5, 10));
-		accountHolderRepository.save(holder1);
-		
-		Account account1 = new Account();
-		account1.setBalance(20.4);
-		account1.setAccountHolder(holder1);
-		accountRepository.save(account1);
+    @Override
+    public void run(String... args) throws Exception {
+        AccountHolder holder1 = new AccountHolder();
+        holder1.setName("Bob");
+        holder1.setBirthday(LocalDate.of(1990, 5, 10));
+        accountHolderRepository.save(holder1);
+    
+        Account account1 = new Account();
+        account1.setBalance(20.4);
+        account1.setAccountHolder(holder1);
+        accountRepository.save(account1);
 
-		
-		AccountHolder holder2 = new AccountHolder();
-		holder2.setName("Amy");
-		holder2.setBirthday(LocalDate.of(1991, 11, 2));
-		accountHolderRepository.save(holder2);
-		
-		Account account2 = new Account();
-		account2.setBalance(30.1);
-		account2.setAccountHolder(holder2);
-		accountRepository.save(account2);
-		
-	}
+    
+        AccountHolder holder2 = new AccountHolder();
+        holder2.setName("Amy");
+        holder2.setBirthday(LocalDate.of(1991, 11, 2));
+        accountHolderRepository.save(holder2);
+    
+        Account account2 = new Account();
+        account2.setBalance(30.1);
+        account2.setAccountHolder(holder2);
+        accountRepository.save(account2);
+    }
 
 }
